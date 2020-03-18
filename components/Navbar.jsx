77b9dd872d4ed1bb4587acc3b0dd162a1cae076d
dongb909 <dongb909@gmail.com>
2020-03-17 20:04:22 -0700

    Create component skeletons

---
+++ components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Nav = () => {
+  return (  
+    <div className="nav">
+      <div>
+        <div>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+ 
+export default Nav;

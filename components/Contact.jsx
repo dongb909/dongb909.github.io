8b74962ba80e089bc6ff6a645f3d5ed3ca45e9e7
dongb909 <dongb909@gmail.com>
2020-03-20 01:50:32 -0700

    Contacts page content skeleton

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -5,16 +5,34 @@ function Contact () {
     <div className='section contact'>
       <div className="contacts_container">
         <div className="contacts_content contacts_heading">
+          <h1>CONNECT WITH ME!</h1>
+          <p>Thank you for visiting!</p>
+          <p>Connect with me and let's collaborate.</p>
+          <img></img>
+        </div>
         <div classNAme="contacts_content contacts_icons">
           <div className="icon">
-
+            <a href="#https://www. .com">Github</a>
           </div>
           <div className="icon">
-
+            <svg fill="#000000" 
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 46 46" width="65" height="65">    <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"/></svg>
+            <a href="#https://www. .com">LinkedIn</a>
           </div>
-          <div className="icon"></div>
-
-        </div>
+          <div className="icon">
+            <p>Email</p>
+          </div>
+          <div className="icon">
+            <p>Phone</p>
+          </div>
+          <div className="icon">
+            <svg height="70" viewBox="0 0 500 500" width="70" 
+            xmlns="http://www.w3.org/2000/svg">
+                <path d="m511.917 189.274h-128.834v-189.274h-271.547l-111.453 111.457v400.543h383v-76.324h128.833v-246.402zm-399.167-148.06v71.456h-71.454zm240.333 440.786h-323v-339.331h112.667v-112.669h210.333v159.274h-283v246.402h283zm128.834-76.324h-381.834v-186.402h381.833v186.402z"/><path d="m162.292 335.761h17.733c24.25 0 43.979-19.729 43.979-43.979s-19.729-43.979-43.979-43.979h-47.733v130.917h30zm0-57.959h17.733c7.708 0 13.979 6.271 13.979 13.979s-6.271 13.979-13.979 13.979h-17.733z"/><path d="m395.763 335.761h41.32v-30h-41.32v-27.959h51.987v-30h-81.987v130.918h30z"/><path d="m339.204 334.233v-42.368c0-24.296-19.767-44.063-44.063-44.063h-47.649v130.494h47.649c24.296 0 44.063-19.766 44.063-44.063zm-61.713-56.431h17.649c7.754 0 14.063 6.309 14.063 14.063v42.368c0 7.754-6.309 14.063-14.063 14.063h-17.649z"/><path d="m178.148 112.669h137.436v30h-137.436z"/></svg>
+            
+            <a href="#">Curriculum Vitae</a>
+          </div>
         </div>
       </div>
     </div>

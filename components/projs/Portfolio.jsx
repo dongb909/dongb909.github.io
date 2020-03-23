9aebbbe34389f8e2bb9132b5f774f06f2c6c6609
dongb909 <dongb909@gmail.com>
2020-03-22 20:13:53 -0700

    Replace goals with portfolio proj

---
+++ components/projs/Portfolio.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Portfolio () {
+  return (
+    <div className='project_container'>
+      <div className="project_content">
+        <div className="project_media">
+          <img 
+            src="https://srpics.s3-us-west-1.amazonaws.com/portfolio+pic.png">
+          </img>
+        </div>
+        <div className="project_description">
+          <a href="https://github.com/dongb909/dongb909.github.io"><h1>PORTFOLIO</h1></a>
+          <p>Congratulations! You already know about this frontend application! 
+            There are many templates that I could have used but I wanted to take 
+            the opportunity to practice with desktop vs mobile compatibility and CSS through the use of SASS.
+          </p>
+        </div>
+      </div>  
+    </div>
+  )
+}
+export default Portfolio;

a99cdbb2feffcf74206549ce18b4875996fe8e6c
dongb909 <dongb909@gmail.com>
2020-03-23 23:16:12 -0700

    About me update

--- components/About.jsx
+++ components/About.jsx
@@ -14,7 +14,7 @@ function About () {
         <p><strong>BUT!</strong> I fell in love with it after the very first time I saw the code I wrote came to life on screen.</p>
         <p>I am either working on a project, leetcoding or working on my personal growth and mental health through reading, meal prepping, listening to podcasts and traveling.</p>
         <p>Making bold moves and solving hard problems are what drive me every day.</p> 
-        <p>Self-awareness, empathy, and interpersonalskills are some of my strongest points. </p>
+        <p>Self-awareness, empathy, and interpersonal skills are some of my strongest points. </p>
         {/* <p>Additionally, with over 5 years' worth of experience working with various interdisciplinary teams of healthcare professionals and patient populations. 
           Therefore, self-awareness, empathy and interpersonal skills are some of my strongest points. Not to mention that my name means "humanitarianism"!</p> */}
         <p>Below are some of the technologies I work with:</p>

80c760d6507b69633b7190685198fd3608d10da4
dongb909 <dongb909@gmail.com>
2021-02-05 17:05:04 -0800

    Upgrade and modify dependencies to get ready for redeployment

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typed from 'react-typed';
 
 function Home () { 
   return ( 
@@ -8,12 +9,12 @@ function Home () {
         <div className="home_content">
           <br/>
           <br/>
-          <h1>Hi! My name is Rachel Dong</h1>
-          <p>Software Engineer | Pharmacist | </p>
-          <p>Avid Reader | Lifelong Learner </p>
+          <h2>Hi! My name is </h2>
+          <h1 id="homeTitle"><Typed strings={["Rachel Dong"]} typeSpeed={100} startDelay={80}/></h1>
+          <h2 id="homeSubtitle"><Typed strings={["Welcome to my page", "I'm a Software Engineer", "Web Developer", "Avid Reader", "AND!!!","Lifelong Learner"]} typeSpeed={60} backSpeed={20} startDelay={200} loop/></h2>
           <br/>
           <br/>
-          <a href="#aboutbuttonlinked"><button>TAKE A LOOK AROUND!</button></a>
+          <a href="#aboutbuttonlinked"><button>ABOUT ME</button></a>
         </div>          
       </div>
     </div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,29 @@
-const path = require('path')
-
+const path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname, 'components/index.jsx'),
+  entry: path.resolve(__dirname, "components/index.jsx"),
   output: {
-    path: path.resolve(__dirname, 'public/'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "public/"),
+    filename: "bundle.js",
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.json', '.css', '.scss']
+    extensions: [".js", ".jsx", ".json", ".css", ".scss"],
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          }
-        }
+        exclude: [/node_modules/],
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
       },
       {
         test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
-  
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
-  mode: 'development'
-}
+  mode: "development",
+};

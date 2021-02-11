2a2d00f23867dd0073c1fbca88b97d88d2f3d260
dongb909 <dongb909@gmail.com>
2021-02-10 20:46:15 -0800

    Change about me description

--- components/About.jsx
+++ components/About.jsx
@@ -26,11 +26,10 @@ function About () {
     <section id='about' className="clearfix">
       <a id="aboutlinked"></a>
       <div id="myPhoto"> <img src="../public/cafe.jpg" alt="My profile picture"/></div>
-      {/* <div id='about_content'> */}
         <div id="aboutDescr" className="one">
           <h1 className="slide_in from_right">About <span>Me</span></h1>
-          <p className="slide_in from_left">Welcome to my page! My name is Rachel and I am a Pharmacist turned Software Developer. Although I loved achieving the goal of being able to empower patients to live healthier lives, after 4 years of working alongside interdisciplinary teams of providers to care for diverse patient populations, I felt that the tools and systems we were using were just not efficient enough nor far-reaching enough to make the impact I wanted to achieve.</p>
-          <p  className="slide_in from_left">I fell in love with software engineering for how forward-thinking and open to change this field is. I was instantly hooked after building my first beautiful and fully scaled microservice and have been excited to be a part of the movement towards innovation and efficient products ever since! No surprise because making bold moves and solving hard problems are what drive me every day.</p> 
+          <p className="slide_in from_left">Welcome to my page! My name is Rachel and I am a Pharmacist turned Software Developer. After working alongside interdisciplinary teams of healthcare providers to care for diverse groups of patient populations for over 4 years, I felt that the tools and systems being used were inefficient and not far-reaching enough. Although I loved being able to empower patients to live healthier lives, I knew I could make a bigger impact by creating tools, solutions, and better interfaces to be used by everyone.</p>
+          <p  className="slide_in from_left">I fell in love with software engineering for how forward-thinking and open to change this field is. I was instantly hooked after building my first beautiful and fully scaled microservice. I am excited to be a part of the movement towards innovation and efficient products because making bold moves and solving hard problems is what drives me every day.</p> 
           <p  className="slide_in from_left">When I'm not coding away, I am either working on my personal growth and mental health through reading, meal prepping, listening to podcasts or traveling.</p>
         </div>
     </section>
